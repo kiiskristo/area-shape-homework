1b928db789e10bc0b7a63f38ba35baddb2593e95
kiiskristo <kiiskristo@gmail.com>
2025-03-12 16:46:30 -0400

    Fixed location

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: '/area-shape-homework',
+  assetPrefix: '/area-shape-homework',
   images: {
     unoptimized: true
   }
